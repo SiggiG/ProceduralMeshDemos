665e253ec227532b476ee2c1c829dbd19806d168
Sigurdur Gunnarsson <siggi@siggig.is>
2016-05-20 22:04:45 +0100

    Basic empty UE4 project with a link to the mesh plugin.

---
+++ Source/ProceduralMeshDemos/ProceduralMeshDemosGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProceduralMeshDemos.h"
+#include "ProceduralMeshDemosGameMode.h"
+
+
+
+
---
+++ Source/ProceduralMeshDemos/ProceduralMeshDemosGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProceduralMeshDemosGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALMESHDEMOS_API AProceduralMeshDemosGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
