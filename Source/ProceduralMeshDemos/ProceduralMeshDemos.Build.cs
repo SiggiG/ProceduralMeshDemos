efdf0855f9ef475e079aa6e78f90f9cfc21c9f3b
Sigurdur Gunnarsson <siggi@siggig.is>
2016-05-21 16:01:54 +0100

    Content examples for ProceduralMesh plugin

--- Source/ProceduralMeshDemos/ProceduralMeshDemos.Build.cs
+++ Source/ProceduralMeshDemos/ProceduralMeshDemos.Build.cs
@@ -6,7 +6,7 @@ public class ProceduralMeshDemos : ModuleRules
 {
 	public ProceduralMeshDemos(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshes" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
