a23dadb142925c514c89f23d0a4260b20ec0e603
SiggiG <siggi@siggig.is>
2017-04-30 22:07:29 -0400

    Updating submodule bat file to fetch 4.15 relevant SHAs

--- UpdateSubmodules.bat
+++ UpdateSubmodules.bat
@@ -4,10 +4,11 @@ git submodule update --init
 echo Updating submodules
 cd Plugins
 cd ProceduralMeshes
-git checkout master
-git pull git@github.com:SiggiG/ProceduralMeshes.git master
+git checkout 4.15-VS2015
+git pull git@github.com:SiggiG/ProceduralMeshes.git 4.15-VS2015
 cd ..
 cd RuntimeMeshComponent
 git checkout master
 git pull git@github.com:Koderz/UE4RuntimeMeshComponent.git master
+git checkout 53a885d
 pause
