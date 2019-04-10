f9899848110fa6c6123ee5d487384ff61ac59e58
SiggiG <siggi@siggig.is>
2019-04-10 18:49:02 +0200

    Adding RMC source files

---
+++ Plugins/RuntimeMeshComponent/Source/RuntimeMeshComponentEditor/RuntimeMeshComponentEditor.Build.cs
@@ -0,0 +1,68 @@
+// Copyright 2016-2018 Chris Conway (Koderz). All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class RuntimeMeshComponentEditor : ModuleRules
+{
+    public RuntimeMeshComponentEditor(ReadOnlyTargetRules rules) : base(rules)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicIncludePaths.AddRange(
+            new string[] {
+                "RuntimeMeshComponentEditor/Public"
+				// ... add public include paths required here ...
+			}
+            );
+
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
+                "RuntimeMeshComponentEditor/Private",
+				// ... add other private include paths required here ...
+			}
+            );
+
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+				// ... add other public dependencies that you statically link with here ...
+                
+            }
+            );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                // ... add private dependencies that you statically link with here ...	
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "RenderCore",
+                "ShaderCore",
+                "RHI",
+                "UnrealEd",
+                "LevelEditor",
+                "PropertyEditor",
+                "RawMesh",
+                "AssetTools",
+                "AssetRegistry",
+                "Projects",
+                "EditorStyle",
+                "InputCore",
+
+                "RuntimeMeshComponent",
+            }
+            );
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
+				// ... add any modules that your module loads dynamically here ...
+			}
+            );
+    }
+}
